f12dd712341965c4c6d4b5994c6accc2c135347b
Abhsihek748 <abhishek.sharma9484@gmail.com>
2021-10-27 12:46:49 +0530

    update repo

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 this is a maoropolo code generator , just use get request on the server .
 
 //new branch
+
+update readme
+
+set up in local machine -> just copy the repo and to npm index.js or nodemon then the server will start at port http://localhost:8081 ;
+
+postman api 👍
+ // this is a test api
+http://localhost:8081/test  get request  ->  for small test result
+
+http://localhost:8081/result get request -> for orignal marcopolo game test cases .
--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const express = require("express");
+const express = require("express"); // require express for server development
 const app = express();
 
-app.use(express.json());
+const generateData =                 // function to generate the data for requires arguments and deafult are 100 numbers and line break at 10
+(item = 100, breakPoints =10)=>{ 
+  let data ='' ;
+  for(let i = 1 ; i< item ;i++){
+      let t = 0;
+    if(i%4==0)
+    { data += 'marco' ; t++; }
+    if(i%7==0)
+    { data+= 'polo' ; t++; }
+    
+    if(t == 0)
+    data+= i+'' ;
 
-app.get('/',function(req,res){
+    data += ',';
+
+    if(i%breakPoints == 0)
+    data += '/r/n  ';
+
+  }
+   return data ;
+} 
+
+app.get('/test',function(req,res){   // test route for api
  
-    let data = '' ;
-    for(let i = 1 ; i< 100 ;i++){
-        let t = 0;
-      if(i%4==0)
-      { data += 'marco' ; t++; }
-      if(i%7==0)
-      { data+= 'polo' ; t++; }
-      
-      if(t == 0)
-      data+= i ;
+  let data = generateData();
+  res.json(data );
+})
 
-      data += ',';
-
-      if(i%10 == 0)
-      data += '/n'+',';
-
-    }
-    console.log(JSON.stringify(data));
-
-  res.json({"data" :data });
+app.get('/result',function(req,res){ // result route for api
+ 
+  let data = generateData(1000000 , 1000); // pass the actual values case for test case
+  res.json(data );
 })
 
 
-app.listen(process.env.PORT||8082, function () {
+app.listen(process.env.PORT||8081, function () {    // server listening on port 8081
     console.log("server started");
+})
+app.use(function (req, res) {    // default page
+    res.status(404).json({
+        message: "page Not found"
+    })
 })
